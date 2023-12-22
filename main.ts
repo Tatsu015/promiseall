5ab85f0c2ad225254a74de7d568995956c8804cd
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-22 20:24:31 +0900

    array change to obj

--- main.ts
+++ main.ts
@@ -4,14 +4,22 @@ const promise3 = new Promise((resolve, _) => {
     setTimeout(resolve, 1000, 'foo');
 });
 
-async function promiseAll(values: any[]) {
+async function promiseAll(obj: {}) {
+    const values = Object.values(obj)
     const results = await Promise.all(values);
     return results
 }
 
 async function main() {
     console.log('[start]', new Date())
-    const results = await promiseAll([promise1, promise2, promise3])
+
+    const obj = {
+        p1: promise1,
+        p2: promise2,
+        p3: promise3,
+    }
+    const results = await promiseAll(obj)
+
     console.log(results)
     console.log('[ end ]', new Date())
 }
