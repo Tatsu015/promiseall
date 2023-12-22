b7569140375b6119dfa7747d4356c81ef14058d2
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-22 20:02:26 +0900

    add promise.all process

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-console.log('hello')
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000, 'foo');
+});
+
+
+console.log('start')
+const results = Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+});
+
+console.log(results)
+
+console.log('end')
