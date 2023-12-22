202015acc3f8c83378ff2735a3c80550eb7fe06d
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-22 20:16:35 +0900

    add timer

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 const promise1 = Promise.resolve(3);
 const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
+const promise3 = new Promise((resolve, _) => {
     setTimeout(resolve, 1000, 'foo');
 });
 
-async function f() {
-    console.log('start')
-    const results = await Promise.all([promise1, promise2, promise3]);
+async function f(values: any[]) {
+    console.log('start', new Date())
+    const results = await Promise.all(values);
 
     console.log('result', results)
 
-    console.log('end')
+    console.log('end  ', new Date())
 }
 
-f()
+f([promise1, promise2, promise3])
