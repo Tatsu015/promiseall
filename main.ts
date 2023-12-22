533516e95b42f947da4eb3cad4271629e4ce135a
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-22 20:20:37 +0900

    rename function

--- main.ts
+++ main.ts
@@ -4,14 +4,14 @@ const promise3 = new Promise((resolve, _) => {
     setTimeout(resolve, 1000, 'foo');
 });
 
-async function f(values: any[]) {
+async function promiseAll(values: any[]) {
     const results = await Promise.all(values);
     return results
 }
 
 async function main() {
     console.log('[start]', new Date())
-    const results = await f([promise1, promise2, promise3])
+    const results = await promiseAll([promise1, promise2, promise3])
     console.log(results)
     console.log('[ end ]', new Date())
 }
