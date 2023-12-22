e62ca1668d69ef75573a3c15b459982d9731f3c9
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-22 20:07:47 +0900

    sync OK

--- main.ts
+++ main.ts
@@ -4,12 +4,13 @@ const promise3 = new Promise((resolve, reject) => {
     setTimeout(resolve, 1000, 'foo');
 });
 
+async function f() {
+    console.log('start')
+    const results = await Promise.all([promise1, promise2, promise3]);
 
-console.log('start')
-const results = Promise.all([promise1, promise2, promise3]).then((values) => {
-    console.log(values);
-});
+    console.log('result', results)
 
-console.log(results)
+    console.log('end')
+}
 
-console.log('end')
+f()
