a2e6df5ffe030fe0919fe69c32e601548a768f93
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-12-23 17:40:45 +0900

    add API call

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
-const promise1: Promise<string> = new Promise((resolve, _) => {
-    setTimeout(resolve, 500, 'hoge');
-});
-const promise2: Promise<string> = new Promise((resolve, _) => {
-    setTimeout(resolve, 700, 'fuga');
-});
-const promise3: Promise<string> = new Promise((resolve, _) => {
-    setTimeout(resolve, 1000, 'piyo');
-});
+// const promise1: Promise<string> = new Promise((resolve, _) => {
+//     console.log('promise1 start')
+//     setTimeout(resolve, 500, 'hoge');
+// });
+// const promise2: Promise<string> = new Promise((resolve, _) => {
+//     console.log('promise2 start')
+//     setTimeout(resolve, 700, 'fuga');
+// });
+// const promise3: Promise<string> = new Promise((resolve, _) => {
+//     console.log('promise3 start')
+//     setTimeout(resolve, 1000, 'piyo');
+// });
+
+const syncFetch = async (url: string): Promise<string> => {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Error! ${res.status}`)
+    }
+    const data = await res.text()
+    return data;
+}
 
 type Promises = Record<string, Promise<any>>
 type Awaiteds = { [K in keyof Promises]: Awaited<Promises[K]> }
@@ -24,10 +36,15 @@ async function promiseAll(promises: Promises): Promise<Awaiteds> {
 async function main() {
     console.log('[start]', new Date())
 
+    // const obj: Promises = {
+    //     p1: promise1,
+    //     p2: promise2,
+    //     p3: promise3,
+    // }
+
     const obj: Promises = {
-        p1: promise1,
-        p2: promise2,
-        p3: promise3,
+        p1: syncFetch('http://worldtimeapi.org/api/timezone/Asia/Tokyo'),
+        p2: syncFetch('http://worldtimeapi.org/api/timezone/America/New_York'),
     }
     const results = await promiseAll(obj)
 
